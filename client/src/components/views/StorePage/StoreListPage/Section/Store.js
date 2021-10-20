ae90887f15767fa90f4bcfd652dd0e736503dc56
bumjin1013 <bumjin1013@naver.com>
2021-10-21 00:47:38 +0900

    상점 거리 표시 삭제

--- client/src/components/views/StorePage/StoreListPage/Section/Store.js
+++ client/src/components/views/StorePage/StoreListPage/Section/Store.js
@@ -1,14 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Col, Card, Rate, Icon } from 'antd';
-import Distance from '../../../../utils/Distance';
+import React from 'react';
+import { Col, Card, Rate } from 'antd';
 import Meta from "antd/lib/card/Meta";
 
 function Store(props) {
 
-    const userAddress = useSelector(state => state.user.userData && state.user.userData.address.address);
     const store = props.store;
-    const [Dis, setDis] = useState();
+
     //별점 계산
     let totalStar = 0;
     
@@ -20,21 +17,6 @@ function Store(props) {
     let Star;
     //리뷰가 1개 이상이면 더한 총 별점 / 리뷰 갯수 = Star , 리뷰가 없으면 0
     store.review.length > 0 ? Star = (totalStar/store.review.length).toFixed(1) : Star = 0
-    
-    //유저와 상점 직선거리 계산
-    const distance = () => {
-        if(store.address && userAddress){
-            return (
-                <Distance storeAddress={store.address} myAddress={userAddress}/>
-            )
-        } else {
-            return (
-                <Icon type="loading" />
-            )
-        }
-    }
-
-    console.log(Dis);
       
     return (
         <Col lg={6} md={8} xs={24} key={store.storeId}>
@@ -55,7 +37,6 @@ function Store(props) {
                         <Rate allowHalf value={Star} disabled={true} key={store.storeId}/>
                         <br/>
                         <h4>{store.title}({Star}) </h4>
-                        {distance()}m
                     </div> 
                 }
             />
